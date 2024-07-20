f1dcb606b6b00d49faf51a0187309f96dc4c1f27
Pablo López Bergillos <plopez230@gmail.com>
2024-07-20 16:56:50 +0200

    Update urls.py

--- django/backend/urls.py
+++ django/backend/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns = [
     path('user/<username>/', views.user_view, name="user"),
+    path('', views.index_view, name="index"),
+    path('invitation/create', views.create_invitation, name="invitation_create"),
+    path('invitation/dismiss', views.dismiss_invitation, name="invitation_dismiss"),
+    path('invitation/accept', views.accept_invitation, name="invitation_accept"),
+    path('invitation/cancel', views.cancel_invitation, name="invitation_cancel"),
 ]
