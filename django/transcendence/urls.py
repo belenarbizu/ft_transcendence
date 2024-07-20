67561f4071401206bd6d20ab98ae90d4b9ae682e
Pablo López Bergillos <plopez230@gmail.com>
2024-07-20 18:07:19 +0200

    Update urls.py

--- django/transcendence/urls.py
+++ django/transcendence/urls.py
@@ -1,13 +1,18 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.i18n import i18n_patterns
+from django.conf.urls.static import static
+from . import settings
 
 urlpatterns = i18n_patterns(
     path('i18n/', include('django.conf.urls.i18n')),
-    path('admin/', admin.site.urls),
     path("", include(("backend.urls", "backend"), namespace="backend")),
+    path('accounts/', include('django.contrib.auth.urls')),
 )
 
 urlpatterns += [
     path('polls/', include('polls.urls')),
+    path('admin/', admin.site.urls),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
