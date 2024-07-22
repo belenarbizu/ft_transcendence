ac09c9abd793a40cfa3503f051e3f36a2df90ce4
Pablo López Bergillos <plopez230@gmail.com>
2024-07-22 22:49:55 +0200

    Update init.sh
    makemigrations backend

--- django/init.sh
+++ django/init.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 python manage.py makemigrations
+python manage.py makemigrations backend
 python manage.py migrate
 python manage.py collectstatic --noinput
 gunicorn --bind 0.0.0.0:8000 transcendence.wsgi:application
