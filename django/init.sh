b09e7577ada86f6c8435afc04b406e61666ef0c4
Pablo López Bergillos <plopez230@gmail.com>
2024-07-22 23:48:24 +0200

    Create command to add test data

--- django/init.sh
+++ django/init.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+# The next command should not be executed in production:
+rm -rf ./backend/migrations
 python manage.py makemigrations
 python manage.py makemigrations backend
 python manage.py migrate
+# The next command should not be executed in production:
+python manage.py demodata
 python manage.py collectstatic --noinput
 gunicorn --bind 0.0.0.0:8000 transcendence.wsgi:application
