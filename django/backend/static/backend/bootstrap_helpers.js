095fc5c5693d3894773f566b22c9233cb56bce5b
Plopez230 <plopez230@gmail.com>
2024-07-28 14:25:19 +0200

    Update bootstrap_helpers.js

--- django/backend/static/backend/bootstrap_helpers.js
+++ django/backend/static/backend/bootstrap_helpers.js
@@ -1,35 +1,15 @@
 
-/**
- * Manages the initialization and disposal of Bootstrap tooltips,
- * and prepares single-page application (SPA) style link handling.
- * 
- * Functions:
- * - create_tooltips(): Initializes Bootstrap tooltips for elements
- *   with the attribute `data-bs-toggle="tooltip"`.
- * 
- * - dispose_tooltips(): Disposes of all initialized Bootstrap tooltips
- *   to free up resources.
- * 
- * Variables:
- * - tooltipList: An array holding references to all initialized
- *   Bootstrap tooltips.
- */
-
 var tooltipList;
 
 function create_tooltips() {
+    tooltipList = [];
     var tooltipTriggerList = document.querySelectorAll(
         '[data-bs-toggle="tooltip"]');
-    tooltipList = [];
-    for (tooltipElement in tooltipTriggerList){
+    tooltipTriggerList.forEach(tooltipElement => {
         try{
             tooltipList.push(new bootstrap.Tooltip(tooltipElement));
         } catch { }
-    }
-
-   // tooltipList = [...tooltipTriggerList].map(
-   //     tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
-
+    });
 }
 
 function dispose_tooltips() {
